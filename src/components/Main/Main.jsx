cae53dea8e87d5f971493174bce5aae3ebc52cfa
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2024-03-13 15:39:19 +0100

    Main 1st commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,6 +10,30 @@ const Main = () => {
             <p>MY AI</p>
             <img src={assets.user_icon} alt="" />
        </div>
+       <div className="main-cointainer">
+            <div className="greet">
+                <p><span>Hello, David</span></p>
+                <p>How can i help you today </p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Describe your ideal environment as a developer</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>How would you address a project delay or error with your supervisor or client? </p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>What are some of your strengths and weaknesses as a developer?</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>What do you do outside of work hours to make yourself a better developer?</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+            </div>
+       </div>
     </div>
   )
 }
