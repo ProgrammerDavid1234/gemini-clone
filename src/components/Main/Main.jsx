61e241b394d4c9a86b31e3721141091860803c11
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2024-03-16 15:10:52 +0100

    main2

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
 
 
 const Main = () => {
+
+
+    const{onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context)
+
   return (
     <div className='main'>
        <div className="nav">
@@ -11,7 +16,10 @@ const Main = () => {
             <img src={assets.user_icon} alt="" />
        </div>
        <div className="main-cointainer">
-            <div className="greet">
+
+            {!showResult
+                ?<>
+                   <div className="greet">
                 <p><span>Hello, David</span></p>
                 <p>How can i help you today </p>
             </div>
@@ -32,6 +40,43 @@ const Main = () => {
                     <p>What do you do outside of work hours to make yourself a better developer?</p>
                     <img src={assets.bulb_icon} alt="" />
                 </div>
+            </div>
+                </>
+                :<div className='result'>
+                    <div className="result-title">
+                        <img src={assets.user_icon} alt="" />
+                        <p>{recentPrompt}</p>
+                    </div>
+                    <div className="result-data">
+                        <img src={assets.gemini_icon} alt="" />
+                        {loading
+                        ?<div className='loader'>
+                            <hr />
+                            <hr />
+                            <hr />
+                        </div>
+                        
+                        :<p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                        
+                        }
+                       
+                    </div>
+                </div>
+            }
+
+         
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here'/>
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className="bottom-info">
+                My AI may display inaccurate info, including about people, so double-check its responses. 
+                </p>
             </div>
        </div>
     </div>
